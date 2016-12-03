8668ab87d10a6440a86ae4c8915c6765674763eb
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-12-03 20:40:45 -0300

    Notification added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { LocalNotifications } from 'ionic-native';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,6 +12,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  Notificate(){
+    LocalNotifications.schedule({
+      id: 1,
+      title: 'ionicThemes',
+      text: 'Single Notification',
+    });
   }
 
 }
